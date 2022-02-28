c6f2765672967d29608ed7e70a32bd735e6002ab
Harvey M <watermelonrouge@gmail.com>
2022-02-27 23:53:25 -0500

    various changes and introduction of py files

---
+++ prepare_dictionary.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import zipfile
+import re
+
+DICT_WIKI_PATH = 'dictionary_lowercase.zip'
+DICT_WORD_PATH = 'wikipedia_en.zip'
+
+dict_wiki = zipfile.ZipFile(DICT_WIKI_PATH)
+dict_wiki = pd.read_csv(dict_wiki.open(re.sub('zip$','txt',DICT_WIKI_PATH)),header=None,names=['word'])
+dict_wiki['word'] = dict_wiki['word'].apply(lambda x: str(x).upper())
+dict_wiki = dict_wiki.drop_duplicates()
+
+dict_word = zipfile.ZipFile(DICT_WORD_PATH)
+dict_word = pd.read_csv(dict_word.open(re.sub('zip$','txt',DICT_WORD_PATH)),sep=' ',header=None,names=['word','len','instances','articles'])
+dict_word['word'] = dict_word['word'].apply(lambda x: str(x).upper())
+dict_word = dict_word.groupby(['word','len']).sum().sort_values(by='instances',ascending=False).reset_index()
+
+dict_master = dict_word.merge(dict_wiki,how='inner',on='word',sort=False)
+dict_master.to_csv('dict_master.zip',compression=dict(method='zip',archive_name='dict_master.csv'))
---
+++ wordle.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import numpy as np
+import zipfile
+import re
+from termcolor import colored
+
+DICT_MAIN_PATH = 'dict_main.zip'
+
+class Wordle:
+    def __init__(self,data,n=5):
+        self.data = data[data['len']==n].reset_index(drop=True)
+        self.word = self.data['word'][np.random.randint(len(self.data))]
+        print('Welcome to \033[1mWORDLE!\n\033[0m')
+
+    def guess(self):
+        t = 1
+        keys = {}
+        colors = ['\033[38;5;254m','\033[38;5;241m','\033[38;5;214m','\033[38;5;36m']
+        for a in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
+            keys[a] = 0
+        match = np.zeros(len(self.word),dtype=int)
+        while min(match) < 3:
+            m = ''
+            myguess = input('Guess '+str(t)+': ')
+            if len(myguess) != len(self.word):
+                print('Please choose a '+str(len(self.word))+'-letter word!')
+                continue
+            else:
+                for c in range(len(myguess)):
+                    if myguess[c] == self.word[c]:
+                        match[c] = keys[myguess[c]] = 3
+                    elif (myguess[c] in self.word) and (myguess[c] != self.word[c]):
+                        match[c] = keys[myguess[c]] = 2
+                    else:
+                        match[c] = keys[myguess[c]] = 1
+                if min(match) != 3:
+                    t += 1
+                for s in myguess:
+                    m += '\033[1m'+colors[keys[s]]+' '+s
+                m += '   :   '
+                for s in keys.keys():
+                    m += colors[keys[s]]+' '+s
+                print(m)
+                if min(match) == 3:
+                    print('you got it in '+str(t)+' tries :)')
+                    break
+
+if __name__ == '__main__':
+    dict_main = pd.read_csv(zipfile.ZipFile(DICT_MAIN_PATH).open(re.sub('zip$','csv',DICT_MAIN_PATH)))
+    word = Wordle(dict_main)
+    word.guess()
+
