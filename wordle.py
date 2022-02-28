44ac9587ccd9e71016d43453de7e78707b027a9f
Harvey M <watermelonrouge@gmail.com>
2022-02-28 06:34:27 -0500

    lots of bug fixes!

--- wordle.py
+++ wordle.py
@@ -10,37 +10,47 @@ class Wordle:
     def __init__(self,data,n=5,first=True):
         if first is True:
             print('Welcome to \033[1mWORDLE!\n\033[0m')
-            self.data = data[data['len']==n].reset_index(drop=True)
+            self.n = n
+            self.data = data[data['len']==self.n].reset_index(drop=True)
         self.word = self.data['word'][np.random.randint(len(self.data))]
+        self.keys = {}
+
+    def set_keys(self,save_letters=False):
+        for a in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
+            if save_letters is False:
+                self.keys[a] = list(np.zeros(self.n+1,dtype=int))
+            else:
+                self.keys[a] = list(np.zeros(self.n,dtype=int))+[self.keys[a][self.n]]
 
     def guess(self):
         t = 1
-        keys = {}
         colors = ['\033[38;5;254m','\033[38;5;239m','\033[38;5;214m','\033[38;5;36m']
-        for a in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
-            keys[a] = 0
-        match = np.zeros(len(self.word),dtype=int)
+        match = np.zeros(self.n,dtype=int)
+        self.set_keys()
         while min(match) < 3:
             m = ''
-            myguess = input('Guess '+str(t)+': ')
-            if len(myguess) != len(self.word):
-                print('Please choose a '+str(len(self.word))+'-letter word!')
+            myguess = input('Guess '+str(t)+' : ')
+            if len(myguess) != self.n:
+                print('Please choose a '+str(self.n)+'-letter word!')
                 continue
             else:
-                for c in range(len(myguess)):
-                    if myguess[c] == self.word[c]:
-                        match[c] = keys[myguess[c]] = 3
-                    elif (myguess[c] in self.word) and (myguess[c] != self.word[c]):
-                        match[c] = keys[myguess[c]] = 2
+                guessmap = []
+                for c in myguess:
+                    guessmap.append(self.keys[c][0])
+                    self.keys[c][0] += 1
+                self.set_keys(save_letters=True)
+                for i,c in enumerate(myguess):
+                    if c == self.word[i]:
+                        match[i] = self.keys[c][guessmap[i]-1] = 3
+                    elif (c in self.word) and (c != self.word[i]):
+                        match[i] = self.keys[c][guessmap[i]-1] = 2
                     else:
-                        match[c] = keys[myguess[c]] = 1
-                if min(match) != 3:
-                    t += 1
-                for s in myguess:
-                    m += '\033[1m'+colors[keys[s]]+s+' '
+                        match[i] = self.keys[c][guessmap[i]-1] = 1
+                    self.keys[c][self.n] = max(self.keys[c][self.n],match[i])
+                    m += '\033[1m'+colors[match[i]]+c+' '+'\033[1m'
                 m += '  :   '
-                for s in keys.keys():
-                    m += colors[keys[s]]+' '+s
+                for s in self.keys.keys():
+                    m += '\033[1m'+colors[self.keys[s][self.n]]+' '+s+'\033[1m'
                 print(m)
                 if min(match) == 3:
                     print('you got it in '+str(t)+' tries :)\n')
@@ -50,6 +60,10 @@ class Wordle:
                         self.guess()
                     else:
                         break
+                else:
+                    self.set_keys(save_letters=True)
+                    t += 1
+        return 'You got it in '+str(t)+' tries :)'
 
 if __name__ == '__main__':
     dict_main = pd.read_csv(zipfile.ZipFile(DICT_DIR+DICT_MAIN_PATH).open(re.sub('zip$','csv',DICT_MAIN_PATH)))
