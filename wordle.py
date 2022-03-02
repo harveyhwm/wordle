225bcddcd9d4766e35cf87ea4f1d0b3828f595bf
Harvey M <watermelonrouge@gmail.com>
2022-03-02 16:45:13 -0500

    method for giving up added into the flow, couple other bug fixes

--- wordle.py
+++ wordle.py
@@ -8,11 +8,14 @@ DICT_DIR = os.path.dirname(os.path.realpath(__file__))+'/data/'
 DICT_MAIN_PATH = 'dict_main.zip'
 
 class Wordle:
-    def __init__(self,data,n=5,first=True):
+    def __init__(self,data,first=True):
         if first is True:
+            self.alldata = data
+            self.breakword = 'IGIVEUP' #string to input if you can't get it and want to find the answer :)
             print('Welcome to \033[1mWORDLE!\n\033[0m')
-            self.n = n
-            self.data = data[data['len']==self.n].reset_index(drop=True)
+        n = input('Please choose your desired word length : ')
+        self.n = int(n)
+        self.data = self.alldata[self.alldata['len']==self.n].reset_index(drop=True)
         self.word = self.data['word'][np.random.randint(len(self.data))]
         self.keys = {}
 
@@ -32,48 +35,50 @@ class Wordle:
         self.set_keys()
         while min(match) < 3:
             m = ''
-            myguess = input('Guess '+str(t)+' : ').upper()
-            if len(myguess) != self.n:
-                print('Please choose a '+str(self.n)+'-letter word!')
+            myguess = input(f'Guess {t} : ').upper()
+            if myguess == self.breakword:
+                print(f'The answer was {self.word} :)\n')
+                match = [3 for i in match]
+            if (len(myguess) != self.n) and (myguess != self.breakword):
+                print(f'Please choose a {self.n}-letter word!')
                 continue
-            elif re.match('^[A-Za-z]{'+str(self.n)+'}$',myguess) is None:
+            elif (re.match('^[A-Za-z]{'+str(self.n)+'}$',myguess) is None) and (myguess != self.breakword):
                 print('Please use alphabetic characters only!')
                 continue
             else:
-                guessmap = []
-                for c in myguess:
-                    guessmap.append(self.keys[c][0])
-                    self.keys[c][0] += 1
-                self.set_keys(save_letters=True)
-                for i,c in enumerate(myguess):
-                    if c == self.word[i]:
-                        match[i] = self.keys[c][guessmap[i]-1] = 3
-                    elif (c in self.word) and (c != self.word[i]):
-                        match[i] = self.keys[c][guessmap[i]-1] = 2
-                    else:
-                        match[i] = self.keys[c][guessmap[i]-1] = 1
-                    self.keys[c][self.n] = max(self.keys[c][self.n],match[i])
-                    m += '\033[1m'+colors[match[i]]+c+' '+'\033[1m'
-                m += '\033[0m  :   '
-                for s in self.keys.keys():
-                    m += '\033[1m'+colors[self.keys[s][self.n]]+' '+s+'\033[1m'
-                m += '\033[0m'
-                print(m)
+                if myguess != self.breakword:
+                    guessmap = []
+                    for c in myguess:
+                        guessmap.append(self.keys[c][0])
+                        self.keys[c][0] += 1
+                    self.set_keys(save_letters=True)
+                    for i,c in enumerate(myguess):
+                        if c == self.word[i]:
+                            match[i] = self.keys[c][guessmap[i]-1] = 3
+                        elif (c in self.word) and (c != self.word[i]):
+                            match[i] = self.keys[c][guessmap[i]-1] = 2
+                        else:
+                            match[i] = self.keys[c][guessmap[i]-1] = 1
+                        self.keys[c][self.n] = max(self.keys[c][self.n],match[i])
+                        m += '\033[1m'+colors[match[i]]+c+' '+'\033[1m'
+                    m += '\033[0m  :   '
+                    for s in self.keys.keys():
+                        m += '\033[1m'+colors[self.keys[s][self.n]]+' '+s+'\033[1m'
+                    m += '\033[0m'
+                    print(m)
                 if min(match) == 3:
-                    print('you got it in '+str(t)+' tries :)\n')
+                    if myguess != self.breakword: print(f'You got it in {t} tries :)\n')
                     replay = input('Do you want to play again? y/n : ')
-                    if replay == 'y':
-                        self.__init__(self.data,first=False)
+                    if replay.upper() == 'Y':
+                        self.__init__(self.alldata,first=False)
                         self.guess()
                     else:
                         break
                 else:
                     self.set_keys(save_letters=True)
                     t += 1
-        return 'You got it in '+str(t)+' tries :)'
 
 if __name__ == '__main__':
     dict_main = pd.read_csv(zipfile.ZipFile(DICT_DIR+DICT_MAIN_PATH).open(re.sub('zip$','csv',DICT_MAIN_PATH)))
     word = Wordle(dict_main)
     word.guess()
-
